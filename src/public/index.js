3f07b41c0e70f0b7e8514262aed959f90b18eb4c
Wyat Soule <wsoule679@gmail.com>
2021-11-30 11:34:30 -0500

    add function to all number buttons, add function to clear button

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+function numberClick(buttonNum){
+    totNum(buttonNum);
+}
+function totNum(addNum){
+    const screenNum = document.getElementById('number-area').innerText;
+    if(screenNum === '0'){
+        const updateNum = addNum.toString();
+        document.getElementById('number-area').innerHTML = updateNum;
+    } else if(screenNum.includes('.')){
+        if(addNum.toString() === '.'){
+            document.getElementById('number-area').innerHTML = screenNum;
+        } else {
+            const updateNum = screenNum + addNum.toString();
+            document.getElementById('number-area').innerHTML = updateNum;
+        }
+    } else {
+        const updateNum = screenNum + addNum;
+        document.getElementById('number-area').innerHTML = updateNum;
+    }
+}
+function clearClick(){
+    document.getElementById('number-area').innerHTML = '0';
+}
